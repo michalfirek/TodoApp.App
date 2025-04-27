beb31ff63ca0eab8f101a50e71f9b27d74d97185
michalfirek <michalfirek@icloud.com>
2025-04-27 18:09:21 +0200

    Implementacja repozytorium Todo i struktury usług
    - Dodano `ToDoRepository` dla operacji CRUD na encjach `Todo` przy użyciu Entity Framework Core.
    - Utworzono `TodoDto` do przesyłania danych `Todo` z właściwościami dla `Id`, `Title`, `Description` i `IsDone`.
    - Zdefiniowano interfejs `ITodoService` do asynchronicznego zarządzania operacjami `Todo`.
    - Zaktualizowano plik projektu, aby odwoływał się do `TodoApp.Data` i usunięto przestarzałe wpisy folderów.

---
+++ TodoApp.Data/Repositories/ToDoRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Data.Context;
+using TodoApp.Data.Model;
+
+namespace TodoApp.Data.Repositories
+{
+	class ToDoRepository
+	{
+		private readonly AppDbContext _context;
+
+		public ToDoRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<Todo>> GetAllAsync()
+		{
+			return await _context.Todos.ToListAsync(); 
+		}
+
+		public async Task<Todo> GetByIdAsync(int id)
+		{
+			return await _context.Todos.FindAsync(id);
+		}
+
+		public async Task AddAsync(Todo todo)
+		{
+			_context.Todos.Add(todo);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task UpdateAsync(Todo todo)
+		{
+			_context.Todos.Update(todo);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			var todo = await GetByIdAsync(id);
+			if (todo != null)
+			{
+				_context.Todos.Remove(todo);
+				await _context.SaveChangesAsync();
+			}
+		}
+	}
+}
---
+++ TodoApp.Services/Interfaces/ITodoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Data;
+using TodoApp.Services.DTOs;
+
+namespace TodoApp.Services.Interfaces
+{
+	interface ITodoService
+	{
+		public Task<List<TodoDto>> GetAllTodosAsync();
+		public Task<TodoDto> GetTodoByIdAsync(int id);
+		public Task AddTodoAsync(TodoDto todo);
+		public Task UpdateTodoAsync(TodoDto todo);
+		public Task DeleteTodoAsync(int id);
+	}
+}
